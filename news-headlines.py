750bc7079857e541a21c80bbb3faeda00cbb8f2a
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2023-05-14 23:20:12 +0900

    Automate The News - Finding Elements

--- news-headlines.py
+++ news-headlines.py
@@ -1,17 +1,47 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import pandas as pd
 
 url = "https://www.thesun.co.uk/sport/football/"
 path = "\Program Files\chromedriver_win32"
 
 service = Service(executable_path=path)
-driver = webdriver.Chrome(service=Service(path))
 
-driver.get(url)
+class WebDriverContext:
+    def __init__(self, service):
+        self.service = service
 
-soccer_news_list = driver.find_elements(by="xpath", value='//div[@class="teaser__copy-container"]')
+    def __enter__(self):
+        self.driver = webdriver.Chrome(service=self.service)
+        return self.driver
 
-for a_soccer_news in soccer_news_list:
-    title = a_soccer_news.find_element(by="xpath", value='./a/h3').text
-    subtitle = a_soccer_news.find_element(by="xpath", value='./a/p').text
-    link = a_soccer_news.find_element(by="xpath", value='./a').get_attribute('href')
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.driver.quit()
+
+with WebDriverContext(service) as driver:
+    driver.get(url)
+
+    soccer_news_containers = driver.find_elements(by="xpath", value='//div[@class="teaser__copy-container"]')
+
+    titles = []
+    subtitles = []
+    links = []
+
+    for soccer_news_container in soccer_news_containers:
+        title = soccer_news_container.find_element(by="xpath", value='./a/h3').text
+        subtitle = soccer_news_container.find_element(by="xpath", value='./a/p').text
+        link = soccer_news_container.find_element(by="xpath", value='./a').get_attribute('href')
+
+        titles.append(title)
+        subtitles.append(subtitle)
+        links.append(link)
+
+    soccer_news = {
+        'title': titles,
+        'subtitle': subtitles,
+        'link': links,
+    }
+
+    df_headlines = pd.DataFrame(soccer_news)
+
+    df_headlines.to_csv('headline.csv')
